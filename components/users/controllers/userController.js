fdfd074d399139864ddd23c7010d288166bc79d9
Duong Phan <duong16628@gmail.com>
2020-12-14 11:12:37 +0700

    change method of check-reset-code + change to eslint formatter

--- components/users/controllers/userController.js
+++ components/users/controllers/userController.js
@@ -1,210 +1,211 @@
-const UserModel = require('../models/userModel');
-const moment = require('moment');
-const UserInfoModel = require('../models/userInfoModel');
-const Utils = require('../../../utils/allUtils');
-const Mailer = require('../../../utils/mailer');
-const shortid = require('shortid');
-const constants = require('../../../common/utils/constants');
-const url = require('url');
-const { checkResetToken } = require('../../../utils/allUtils');
+const UserModel = require('../models/userModel')
+const moment = require('moment')
+const UserInfoModel = require('../models/userInfoModel')
+const Utils = require('../../../utils/allUtils')
+const Mailer = require('../../../utils/mailer')
+// const shortid = require('shortid')
+const constants = require('../../../common/utils/constants')
+// const url = require('url')
+const { checkResetToken } = require('../../../utils/allUtils')
 
-const jwtSecret = process.env.jwt_secret;
+const jwtSecret = process.env.jwt_secret
 
 /**
  * Register user
  */
 exports.create = async (req, res) => {
-    req.body.password = Utils.generatePassword(req.body.password);
-    req.body.permissionLevel = 1;
-    const user = await UserModel.create(req.body);
-    if (!user) {
-        return res
-            .status(200)
-            .send(
-                Utils.buildErrorResponse(
-                    res.__('An unexpected error occurred during saving user.'),
-                    constants.ERROR_CODE.SAVE_ERROR,
-                ),
-            );
-    }
-    setImmediate(() => {
-        Mailer.sendActivationEmail(req, user);
-    });
-    Utils.responseUser(user);
-    return res.status(201).send(Utils.buildDataResponse({ data: user }));
-};
+  req.body.password = Utils.generatePassword(req.body.password)
+  req.body.permissionLevel = 1
+  const user = await UserModel.create(req.body)
+  if (!user) {
+    return res
+      .status(200)
+      .send(
+        Utils.buildErrorResponse(
+          res.__('An unexpected error occurred during saving user.'),
+          constants.ERROR_CODE.SAVE_ERROR
+        )
+      )
+  }
+  setImmediate(() => {
+    Mailer.sendActivationEmail(req, user)
+  })
+  Utils.responseUser(user)
+  return res.status(201).send(Utils.buildDataResponse({ data: user }))
+}
 
 /**
  * Resent Activation Email
  */
 
 exports.resentActivationEmail = async (req, res) => {
-    const { email } = req.body;
-    const user = await UserModel.findByEmail(email);
-    if (!user) {
-        return res
-            .status(200)
-            .send(
-                Utils.buildErrorResponse(
-                    res.__('User not found!'),
-                    constants.ERROR_CODE.USER_NOT_FOUND,
-                ),
-            );
-    }
-    setImmediate(() => {
-        Mailer.sendActivationEmail(req, user);
-    });
+  const { email } = req.body
+  const user = await UserModel.findByEmail(email)
+  if (!user) {
     return res
-        .status(201)
-        .send(Utils.buildDataResponse({ msg: res.__('Sent succeeded!') }));
-};
+      .status(200)
+      .send(
+        Utils.buildErrorResponse(
+          res.__('User not found!'),
+          constants.ERROR_CODE.USER_NOT_FOUND
+        )
+      )
+  }
+  setImmediate(() => {
+    Mailer.sendActivationEmail(req, user)
+  })
+  return res
+    .status(201)
+    .send(Utils.buildDataResponse({ msg: res.__('Sent succeeded!') }))
+}
 
 /**
  * Active account after registered
  */
 exports.activate = async (req, res) => {
-    const userId = req.params.userId;
-    const code = req.params.code;
+  const userId = req.params.userId
+  const code = req.params.code
 
-    if (!code || !userId) {
-        return res
-            .status(200)
-            .send(
-                Utils.buildErrorResponse(
-                    res.__('Invalid params.'),
-                    constants.ERROR_CODE.INVALID,
-                ),
-            );
-    }
+  if (!code || !userId) {
+    return res
+      .status(200)
+      .send(
+        Utils.buildErrorResponse(
+          res.__('Invalid params.'),
+          constants.ERROR_CODE.INVALID
+        )
+      )
+  }
 
-    const salt = code.split('$')[0];
-    const checkCode = Utils.generatePassword(userId + jwtSecret, salt, 'hex');
+  const salt = code.split('$')[0]
+  const checkCode = Utils.generatePassword(userId + jwtSecret, salt, 'hex')
 
-    if (checkCode !== code) {
-        return res
-            .status(200)
-            .send(
-                Utils.buildErrorResponse(
-                    res.__('Invalid activation code.'),
-                    constants.ERROR_CODE.INVALID,
-                ),
-            );
-    }
+  if (checkCode !== code) {
+    return res
+      .status(200)
+      .send(
+        Utils.buildErrorResponse(
+          res.__('Invalid activation code.'),
+          constants.ERROR_CODE.INVALID
+        )
+      )
+  }
 
-    const user = await UserModel.update(userId, { activated_at: new Date() });
+  const user = await UserModel.update(userId, { activated_at: new Date() })
 
-    if (!user) {
-        return res
-            .status(200)
-            .send(
-                Utils.buildErrorResponse(
-                    res.__('An unexpected error occured during updating user.'),
-                    constants.ERROR_CODE.SAVE_ERROR,
-                ),
-            );
-    }
+  if (!user) {
+    return res
+      .status(200)
+      .send(
+        Utils.buildErrorResponse(
+          res.__('An unexpected error occured during updating user.'),
+          constants.ERROR_CODE.SAVE_ERROR
+        )
+      )
+  }
 
-    const agent = req.headers['user-agent'];
-    if (agent.includes('Android')) {
-        return res.redirect(
-            'intent://activate#Intent;scheme=globics;package=com.app.globics;end',
-        );
-    } else if (agent.includes('iPhone')) {
-        return res.redirect('globics://activate');
-    } else {
-        return res.redirect(`${process.env.web_url}`);
-    }
-};
+  const agent = req.headers['user-agent']
+  if (agent.includes('Android')) {
+    return res.redirect(
+      'intent://activate#Intent;scheme=globics;package=com.app.globics;end'
+    )
+  } else if (agent.includes('iPhone')) {
+    return res.redirect('globics://activate')
+  } else {
+    return res.redirect(`${process.env.web_url}`)
+  }
+}
 
 exports.verifyEmail = async (req, res) => {
-    const userId = req.params.userId;
-    const email = req.params.email;
-    const code = req.params.code;
+  const userId = req.params.userId
+  const email = req.params.email
+  const code = req.params.code
 
-    const salt = code.split('$')[0];
-    const checkCode = Utils.generatePassword(
-        userId + email + jwtSecret,
-        salt,
-        'hex',
-    );
-    if (checkCode != code) {
-        return res
-            .status(200)
-            .send(
-                Utils.buildErrorResponse(
-                    res.__('Invalid verification code.'),
-                    constants.ERROR_CODE.INVALID,
-                ),
-            );
-    }
-    const existedEmail = await UserModel.findByEmail(email);
-    if (existedEmail)
-        return res
-            .status(200)
-            .send(
-                Utils.buildErrorResponse(
-                    res.__('Email already in use'),
-                    constants.ERROR_CODE.EMAIL_USED,
-                ),
-            );
-    UserModel.update(userId, { email: email });
+  const salt = code.split('$')[0]
+  const checkCode = Utils.generatePassword(
+    userId + email + jwtSecret,
+    salt,
+    'hex'
+  )
+  if (checkCode != code) {
+    return res
+      .status(200)
+      .send(
+        Utils.buildErrorResponse(
+          res.__('Invalid verification code.'),
+          constants.ERROR_CODE.INVALID
+        )
+      )
+  }
+  const existedEmail = await UserModel.findByEmail(email)
+  if (existedEmail) {
+    return res
+      .status(200)
+      .send(
+        Utils.buildErrorResponse(
+          res.__('Email already in use'),
+          constants.ERROR_CODE.EMAIL_USED
+        )
+      )
+  }
+  UserModel.update(userId, { email: email })
 
-    return res.status(200).send(
-        Utils.buildDataResponse({
-            msg: res.__('Your email has been updated successfully!'),
-        }),
-    );
-};
+  return res.status(200).send(
+    Utils.buildDataResponse({
+      msg: res.__('Your email has been updated successfully!')
+    })
+  )
+}
 
 /**
  * Send email when forgot password
  * @param { email : string } req
  */
 exports.sendResetPasswordToken = async (req, res) => {
-    const { email } = req.body;
-    const user = await UserModel.findByEmail(email);
+  const { email } = req.body
+  const user = await UserModel.findByEmail(email)
 
-    if (!user) {
-        return res
-            .status(302)
-            .send(
-                Utils.buildErrorResponse(
-                    res.__("E-mail doesn't exist."),
-                    constants.ERROR_CODE.EMAIL_NO_EXISTS,
-                ),
-            );
-    } else {
-        const expired = moment().add(5, 'minutes');
-        const encrypted = Utils.encryptResetCode(
-            JSON.stringify({ expired, user }),
-        );
-        const serverName = req.headers.host.split(':')[0];
-        const resetLink = `http://${serverName}:3000/change-password/${encrypted.iv}-${encrypted.encryptedData}`;
-        Mailer.sendResetPasswordEmail(req, user, resetLink);
-        return res.status(200).send(
-            Utils.buildDataResponse({
-                msg: {
-                    success:
-                        'We have sent you an email to reset your password.',
-                },
-            }),
-        );
-    }
-};
+  if (!user) {
+    return res
+      .status(302)
+      .send(
+        Utils.buildErrorResponse(
+          res.__("E-mail doesn't exist."),
+          constants.ERROR_CODE.EMAIL_NO_EXISTS
+        )
+      )
+  } else {
+    const expired = moment().add(5, 'minutes')
+    const encrypted = Utils.encryptResetCode(
+      JSON.stringify({ expired, user })
+    )
+    const serverName = req.headers.host.split(':')[0]
+    const resetLink = `http://${serverName}:3000/change-password/${encrypted.iv}-${encrypted.encryptedData}`
+    Mailer.sendResetPasswordEmail(req, user, resetLink)
+    return res.status(200).send(
+      Utils.buildDataResponse({
+        msg: {
+          success:
+                        'We have sent you an email to reset your password.'
+        }
+      })
+    )
+  }
+}
 
 /**
  * Check valid of reset code
  * @param {code: string} req
  */
 exports.checkResetPasswordCode = (req, res) => {
-    const { code } = req.body;
+  const { code } = req.body
 
-    return res.status(200).send(
-        Utils.buildDataResponse({
-            msg: { isExpired: checkResetToken(code).isExpired },
-        }),
-    );
-};
+  return res.status(200).send(
+    Utils.buildDataResponse({
+      msg: { isExpired: checkResetToken(code).isExpired }
+    })
+  )
+}
 
 /**
  * User reset password from forgot password email.
@@ -212,142 +213,144 @@ exports.checkResetPasswordCode = (req, res) => {
  * @param { newPassword : string } req.body
  */
 exports.resetPassword = async (req, res) => {
-    const { code, newPassword } = req.body;
-    const decryptedData = checkResetToken(code);
-    const { decrypted, isExpired } = decryptedData;
+  const { code, newPassword } = req.body
+  const decryptedData = checkResetToken(code)
+  const { decrypted, isExpired } = decryptedData
 
-    if (!code || !newPassword) {
-        return res
-            .status(200)
-            .send(
-                Utils.buildErrorResponse(
-                    res.__('Invalid params.'),
-                    constants.ERROR_CODE.INVALID,
-                ),
-            );
-    } else if (!isExpired) {
-        const hashPassword = Utils.generatePassword(newPassword);
-        const user = await UserModel.update(decrypted.user.id, {
-            password: hashPassword,
-        });
-        if (!user) {
-            return res
-                .status(200)
-                .send(
-                    Utils.buildErrorResponse(
-                        res.__(
-                            'An unexpected error occured during updating password.',
-                        ),
-                        constants.ERROR_CODE.SAVE_ERROR,
-                    ),
-                );
-        } else
-            return res.status(200).send(
-                Utils.buildDataResponse({
-                    msg: { success: 'Update user password successed' },
-                }),
-            );
-    } else
-        return res.status(200).send(
-            Utils.buildDataResponse({
-                msg: { error: 'reset code expired' },
-            }),
-        );
-};
+  if (!code || !newPassword) {
+    return res
+      .status(200)
+      .send(
+        Utils.buildErrorResponse(
+          res.__('Invalid params.'),
+          constants.ERROR_CODE.INVALID
+        )
+      )
+  } else if (!isExpired) {
+    const hashPassword = Utils.generatePassword(newPassword)
+    const user = await UserModel.update(decrypted.user.id, {
+      password: hashPassword
+    })
+    if (!user) {
+      return res
+        .status(200)
+        .send(
+          Utils.buildErrorResponse(
+            res.__(
+              'An unexpected error occured during updating password.'
+            ),
+            constants.ERROR_CODE.SAVE_ERROR
+          )
+        )
+    } else {
+      return res.status(200).send(
+        Utils.buildDataResponse({
+          msg: { success: 'Update user password successed' }
+        })
+      )
+    }
+  } else {
+    return res.status(200).send(
+      Utils.buildErrorResponse({
+        msg: { error: 'reset code expired' }
+      })
+    )
+  }
+}
 
 exports.list = async (req, res) => {
-    const limit = req.query.limit;
-    const page = req.query.page;
-    let role = req.query.role;
-    if (role) {
-        role = role.split(',');
-    }
-    const result = await UserModel.list(role, limit, page);
+  const limit = req.query.limit
+  const page = req.query.page
+  let role = req.query.role
+  if (role) {
+    role = role.split(',')
+  }
+  const result = await UserModel.list(role, limit, page)
 
-    return res.status(200).send(Utils.buildDataResponse({ data: result }));
-};
+  return res.status(200).send(Utils.buildDataResponse({ data: result }))
+}
 
 exports.search = async (req, res) => {
-    const limit = req.query.limit;
-    const page = req.query.page;
-    const search = req.query.search;
+  const limit = req.query.limit
+  const page = req.query.page
+  const search = req.query.search
 
-    const result = await UserModel.search(limit, page, search);
+  const result = await UserModel.search(limit, page, search)
 
-    return res.status(200).send(Utils.buildDataResponse({ data: result }));
-};
+  return res.status(200).send(Utils.buildDataResponse({ data: result }))
+}
 
 exports.getById = async (req, res) => {
-    const userInfo = await UserInfoModel.findByUserId(req.params.userId);
-    const user = await UserModel.findById(req.params.userId);
+  const userInfo = await UserInfoModel.findByUserId(req.params.userId)
+  const user = await UserModel.findById(req.params.userId)
 
-    if (!user) {
-        return res
-            .status(200)
-            .send(
-                Utils.buildErrorResponse(
-                    res.__('User not found.'),
-                    constants.ERROR_CODE.USER_NOT_FOUND,
-                ),
-            );
-    }
+  if (!user) {
+    return res
+      .status(200)
+      .send(
+        Utils.buildErrorResponse(
+          res.__('User not found.'),
+          constants.ERROR_CODE.USER_NOT_FOUND
+        )
+      )
+  }
 
-    Utils.responseUser(user);
-    return res.status(200).send(
-        Utils.buildDataResponse({
-            data: { user: { ...user.toJSON(), userInfo } },
-        }),
-    );
-};
+  Utils.responseUser(user)
+  return res.status(200).send(
+    Utils.buildDataResponse({
+      data: { user: { ...user.toJSON(), userInfo } }
+    })
+  )
+}
 
 exports.changePassword = async (req, res) => {
-    let user = await UserModel.findById(req.params.userId);
-    if (user) {
-        const salt = user.get('password').split('$')[0];
-        const checkNewPassword = Utils.generatePassword(
-            req.body.password,
-            salt,
-        );
-        if (checkNewPassword === user.get('password')) {
-            return res
-                .status(200)
-                .send(
-                    Utils.buildErrorResponse(
-                        res.__(
-                            "New password shouldn't be similar with the current.",
-                        ),
-                        constants.ERROR_CODE.SAME_CURRENT_PASSWORD,
-                    ),
-                );
-        }
-    }
-
-    const newPassword = Utils.generatePassword(req.body.password);
-
-    user = await UserModel.update(req.params.userId, { password: newPassword });
-
-    if (!user) {
-        return res
-            .status(200)
-            .send(
-                Utils.buildErrorResponse(
-                    res.__(
-                        'An unexpected error occured during updating password.',
-                    ),
-                    constants.ERROR_CODE.SAVE_ERROR,
-                ),
-            );
-    }
-
-    return res
+  let user = await UserModel.findById(req.params.userId)
+  if (user) {
+    const salt = user.get('password').split('$')[0]
+    const checkNewPassword = Utils.generatePassword(
+      req.body.password,
+      salt
+    )
+    if (checkNewPassword === user.get('password')) {
+      return res
         .status(200)
-        .send(Utils.buildDataResponse({ msg: res.__('Update succeeded!') }));
-};
+        .send(
+          Utils.buildErrorResponse(
+            res.__(
+              "New password shouldn't be similar with the current."
+            ),
+            constants.ERROR_CODE.SAME_CURRENT_PASSWORD
+          )
+        )
+    }
+  }
+
+  const newPassword = Utils.generatePassword(req.body.password)
+
+  user = await UserModel.update(req.params.userId, { password: newPassword })
+
+  if (!user) {
+    return res
+      .status(200)
+      .send(
+        Utils.buildErrorResponse(
+          res.__(
+            'An unexpected error occured during updating password.'
+          ),
+          constants.ERROR_CODE.SAVE_ERROR
+        )
+      )
+  }
+
+  return res
+    .status(200)
+    .send(Utils.buildDataResponse({ msg: res.__('Update succeeded!') }))
+}
 
 exports.removeById = (req, res) => {
-    UserModel.removeById(req.params.userId).then((result) => {
-        res.status(200).send(
-            Utils.buildDataResponse({ msg: res.__('Delete succeeded!') }),
-        );
-    });
-};
+  UserModel.removeById(req.params.userId).then((result) => {
+    res.status(200).send(
+      Utils.buildDataResponse({ msg: res.__('Delete succeeded!') })
+    )
+  })
+}
--- utils/mailer.js
+++ utils/mailer.js
@@ -1,91 +1,91 @@
-const nodemailer = require('nodemailer');
-const Utils = require('./allUtils');
-const jwtSecret = process.env.jwt_secret;
-const url = require('url');
+const nodemailer = require('nodemailer')
+const Utils = require('./allUtils')
+const jwtSecret = process.env.jwt_secret
+const url = require('url')
 
-const { mailer_user, mailer_password, mailer_name } = process.env;
+const { mailer_user, mailer_password, mailer_name } = process.env
 const sendEmail = async (mailOptions) => {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: mailer_user,
-            pass: mailer_password,
-        },
-    });
-    try {
-        let info = await transporter.sendMail(mailOptions);
-        console.info('Email sent: %s', info.messageId);
-        return true;
-    } catch (e) {
-        console.error(e);
-        return false;
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: mailer_user,
+      pass: mailer_password
     }
-};
+  })
+  try {
+    const info = await transporter.sendMail(mailOptions)
+    console.info('Email sent: %s', info.messageId)
+    return true
+  } catch (e) {
+    console.error(e)
+    return false
+  }
+}
 
 exports.sendActivationEmail = async (req, user) => {
-    let activateCode = Utils.generatePassword(
-        user.get('id') + jwtSecret,
-        null,
-        'hex',
-    );
-    let activateURL = `${Utils.fullUrl(req)}/activate/${user.get(
-        'id',
-    )}/${activateCode}`;
+  const activateCode = Utils.generatePassword(
+    user.get('id') + jwtSecret,
+    null,
+    'hex'
+  )
+  const activateURL = `${Utils.fullUrl(req)}/activate/${user.get(
+        'id'
+    )}/${activateCode}`
 
-    console.info('Activation Link: ' + activateURL);
+  console.info('Activation Link: ' + activateURL)
 
-    // send mail with defined transport object
-    let mailOptions = {
-        from: mailer_name, // sender address
-        to: user.get('email'), // list of receivers
-        subject: 'Globics Customer Account Activation', // Subject line
-        html: `<h3>Thanks for signing up for Globics!</h3> </br>Please activate your account at <a href="${activateURL}">Activate Your Account</a>`,
-    };
+  // send mail with defined transport object
+  const mailOptions = {
+    from: mailer_name, // sender address
+    to: user.get('email'), // list of receivers
+    subject: 'Globics Customer Account Activation', // Subject line
+    html: `<h3>Thanks for signing up for Globics!</h3> </br>Please activate your account at <a href="${activateURL}">Activate Your Account</a>`
+  }
 
-    for (let i = 0; i < 3; i++) {
-        if (await sendEmail(mailOptions)) {
-            break;
-        } else {
-            console.error(`Error (${i}): send email to ${mailOptions.to}`);
-            if (i < 2) await Utils.timeout(60000);
-        }
+  for (let i = 0; i < 3; i++) {
+    if (await sendEmail(mailOptions)) {
+      break
+    } else {
+      console.error(`Error (${i}): send email to ${mailOptions.to}`)
+      if (i < 2) await Utils.timeout(60000)
     }
-};
+  }
+}
 
 exports.sendResetPasswordEmail = (req, user, resetURL) => {
-    // console.info('Reset Password Link: ' + resetURL);
+  // console.info('Reset Password Link: ' + resetURL);
 
-    let mailOptions = {
-        from: mailer_name, // sender address
-        to: user.get('email'), // list of receivers
-        subject: 'Globics Reset Your Password', // Subject line
-        html: `You are receiving this email because we received a password request for your account. 
+  const mailOptions = {
+    from: mailer_name, // sender address
+    to: user.get('email'), // list of receivers
+    subject: 'Globics Reset Your Password', // Subject line
+    html: `You are receiving this email because we received a password request for your account. 
         Click on this link to <a href="${resetURL}">Reset Your Password</a>.
-        Please be notice that the reset link will be expired in 5 minutes, exceeded this time please request another one`,
-    };
+        Please be notice that the reset link will be expired in 5 minutes, exceeded this time please request another one`
+  }
 
-    sendEmail(mailOptions);
-};
+  sendEmail(mailOptions)
+}
 
 exports.mailToVerifyEmail = async (req, userId, receiverEmail) => {
-    let code = Utils.generatePassword(
-        userId + receiverEmail + jwtSecret,
-        null,
-        'hex',
-    );
-    let path = `/api/users/verify/${userId}/${receiverEmail}/${code}`;
-    let verifyUrl = url.format({
-        protocol: 'https',
-        host: req.get('host'),
-        pathname: path,
-    });
+  const code = Utils.generatePassword(
+    userId + receiverEmail + jwtSecret,
+    null,
+    'hex'
+  )
+  const path = `/api/users/verify/${userId}/${receiverEmail}/${code}`
+  const verifyUrl = url.format({
+    protocol: 'https',
+    host: req.get('host'),
+    pathname: path
+  })
 
-    let mailOptions = {
-        from: mailer_name, // sender address
-        to: receiverEmail, // list of receivers
-        subject: 'Verify Your New Email Address', // Subject line
-        html: `<a href="${verifyUrl}">Verify your email address</a>`,
-    };
-    sendEmail(mailOptions);
-};
+  const mailOptions = {
+    from: mailer_name, // sender address
+    to: receiverEmail, // list of receivers
+    subject: 'Verify Your New Email Address', // Subject line
+    html: `<a href="${verifyUrl}">Verify your email address</a>`
+  }
+  sendEmail(mailOptions)
+}
